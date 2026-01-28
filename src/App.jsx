c303885730bc9d263fd65602b07fc8ebd930dc99
Anurag Vishwa <anuragvishwacs@gmail.com>
2026-01-28 20:03:20 +0530

    added api key

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { Navbar } from './components/Navbar';
-import { Hero } from './components/Hero';
-import { LiveVoiceDemo } from './components/LiveVoiceDemo';
-import { Services } from './components/Services';
-import { WhyExists } from './components/WhyExists';
-import { Features } from './components/Features';
-import { HowItWorks } from './components/HowItWorks';
-import { Pricing } from './components/Pricing';
-import { FAQ } from './components/FAQ';
-import { CTA } from './components/CTA';
-import { Footer } from './components/Footer';
-import { ParticleBackground } from './components/ParticleBackground';
+import { Navbar } from "./components/Navbar";
+import { Hero } from "./components/Hero";
+import { LiveVoiceDemo } from "./components/LiveVoiceDemo";
+import { Services } from "./components/Services";
+import { WhyExists } from "./components/WhyExists";
+import { Features } from "./components/Features";
+import { HowItWorks } from "./components/HowItWorks";
+import { Pricing } from "./components/Pricing";
+import { FAQ } from "./components/FAQ";
+import { CTA } from "./components/CTA";
+import { Footer } from "./components/Footer";
+import { ParticleBackground } from "./components/ParticleBackground";
 
 function App() {
   return (
@@ -31,5 +31,5 @@ function App() {
     </div>
   );
 }
-
+//vishwa
 export default App;
